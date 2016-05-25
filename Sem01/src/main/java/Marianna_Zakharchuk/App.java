2f9875426ad9f4c24fd4395e0b697d460bfef25d
mariolla <mariolla1@mail.ru>
2016-05-25 20:20:56 +0200

    Seminar01

---
+++ Sem01/src/main/java/Marianna_Zakharchuk/App.java
@@ -0,0 +1,11 @@
+package Marianna_Zakharchuk;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println();
+        
+    }
+}
